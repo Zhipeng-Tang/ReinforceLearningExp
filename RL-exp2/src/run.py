33407ab3de692785f0d794b37f4fb2fb1916e938
Serandt <tangzhipeng@mail.ustc.edu.cn>
2023-12-25 14:58:31 +0800

    add few args to running script

--- RL-exp2/src/run.py
+++ RL-exp2/src/run.py
@@ -4,14 +4,16 @@ import argparse
 parser = argparse.ArgumentParser(description='running script of DQN')
 parser.add_argument('-e', '--env', type=str, default='CartPole-v1', help='which enviroment, CartPole-v1 or MountainCar-v0')
 parser.add_argument('-d', '--device', type=int, default=0, help='which GPU to use')
+parser.add_argument('--lr', type=float, default=0.00025, help='learning rate')
+parser.add_argument('--epsilon_start', type=float, default=0.5, help='epsilon starting')
 args = parser.parse_args()
 assert args.env in ['CartPole-v1', 'MountainCar-v0']
 assert 0 <= args.device <= 7
 
-cmds = ['CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --epsilon_start 0.95'.format(args.device, args.env),
-        'CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --double --epsilon_start 0.95'.format(args.device, args.env),
-        'CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --dueling --epsilon_start 0.95'.format(args.device, args.env),
-        'CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --double --dueling --epsilon_start 0.95'.format(args.device, args.env),]
+cmds = ['CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --lr {} --epsilon_start {}'.format(args.device, args.env, args.lr, args.epsilon_start),
+        'CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --double --lr {} --epsilon_start {}'.format(args.device, args.env, args.lr, args.epsilon_start),
+        'CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --dueling --lr {} --epsilon_start {}'.format(args.device, args.env, args.lr, args.epsilon_start),
+        'CUDA_VISIBLE_DEVICES={} python DQN.py -e {} --double --dueling --lr {} --epsilon_start {}'.format(args.device, args.env, args.lr, args.epsilon_start),]
 
 for cmd in cmds:
     os.system(cmd)
